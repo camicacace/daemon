e92ab46aaaa31b53e69de95969c8610d7cb46b28
camila <camila@debian.debian>
2024-04-23 16:23:40 -0300

    carga de archivo log

--- mattdeamon.c
+++ mattdeamon.c
@@ -5,6 +5,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <syslog.h>
+#include <fcntl.h>
 
 static void skeleton_daemon()
 {
@@ -20,7 +21,7 @@ static void skeleton_daemon()
 
     if (setsid() < 0)
         exit(EXIT_FAILURE);
-  
+ 
     signal(SIGCHLD, SIG_IGN);
     signal(SIGHUP, SIG_IGN);
 
@@ -28,7 +29,7 @@ static void skeleton_daemon()
 
     if (pid < 0)
         exit(EXIT_FAILURE);
-    
+   
     if (pid > 0)
         exit(EXIT_SUCCESS);
    
@@ -37,7 +38,7 @@ static void skeleton_daemon()
 
     /* Directorio */
     chdir("/");
-    
+   
     /* Cierra todo los archivos que quedaron abiertos por el padre */
     int x;
     for (x = sysconf(_SC_OPEN_MAX); x>=0; x--)
@@ -45,14 +46,28 @@ static void skeleton_daemon()
         close (x);
     }
 
-    openlog ("firstdaemon", LOG_PID | LOG_LOCAL1, LOG_DAEMON);
-    
+    // Abre el archivo de log en modo de adición.
+    // Si el archivo no existe, lo crea.
+    int fd = open("/home/camila/Git/daemon/daemon/daemon.log", O_WRONLY | O_CREAT | O_APPEND, 0644);
+
+    if (fd == -1) {
+        // Maneja el error...
+        exit(EXIT_FAILURE);
+    }
+
+    // Redirige stdout y stderr al archivo de log.
+    dup2(fd, STDOUT_FILENO);
+    dup2(fd, STDERR_FILENO);
+
+    // No es necesario cerrar fd aquí, se cerrará cuando el proceso termine.
 }
 
 int main()
 {
-    
-    //Funcionalidad
-
+    // Llama a skeleton_daemon para crear el daemon
+    skeleton_daemon();
+   
+    // Aquí va la funcionalidad del daemon...
+    printf("Main");
     return EXIT_SUCCESS;
 }
